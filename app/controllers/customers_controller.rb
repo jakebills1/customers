7add53d86a750a8c1b78a2461fe34b8e80efb7d7
Jake Bills <jakebills1@gmail.com>
2019-02-28 11:34:40 -0700

    add sql methods for index and new

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,40 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
   def index
+    # Active record
+    # @customer = Customer.all
+
+    # find_by_sql - SELECT all records
+    @customers = Customer.all_customers(current_user.id)
   end
 
   def show
   end
 
   def new
+    @customer = current_user.customers.new
+  end
+
+  def create
+    # Active record
+    # @customer = current_user.customers.new(customer_params)
+    Customer.create_customer(customer_params, current_user.id)
   end
 
   def edit
   end
+
+  private
+    def set_customer
+      # active record
+      # @customer = Customer.find(params[:id])
+
+      # find_by_sql - SELECT single record
+      @customer = Customer.single_customer(current_user.id, params[:id])
+    end
+
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :email, :phone)
+    end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,36 @@
 class Customer < ApplicationRecord
   belongs_to :user
+
+  # instance methods
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+
+  # class methods
+  def self.all_customers(id)
+    Customer.find_by_sql(
+      "
+      SELECT * 
+      FROM customers AS c
+      WHERE c.user_id = #{id}
+      "
+    )
+  end
+
+  def self.single_customer(user_id, customer_id)
+    # ["query", arg1, arg2]
+    Customer.find_by_sql([
+      "
+      SELECT *
+      FROM customers AS c
+      WHERE c.id = ?
+      ",
+      customer_id])
+  end
+
+  def self.create_customer(p, id)
+    
+
+  end
 end
